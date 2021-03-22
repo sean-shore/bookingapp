fcbdd5efcbd8efd765f57920f32192720b9489ba
seanshore <sshore@resharmonics.co.uk>
2021-03-22 20:27:31 +0000

    19 march

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from "@angular/common/http";
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { ApiConstant } from '..';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  token: string;
+  baseUrl: string;
+
+  constructor(
+    private httpClient:HttpClient,
+  ) { }
+
+  login(credentials, done: (token: string) => void, fail: (err: any) => void) {
+    this.setBaseURL(credentials.username, credentials.password);
+    let authUrl = `${this.baseUrl}/auth?username=${credentials.username}&password=${credentials.password}`;
+  }
+
+}
