2a43cc882737541fbadaf7e7834e6cf95c9a736c
seanshore <sshore@resharmonics.co.uk>
2021-03-23 13:52:55 +0000

    23 march

---
+++ src/app/login-form/login-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthService} from "../auth.service";
+
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+  username: string;
+  password: string;
+
+  constructor(
+    private authService: AuthService) {
+  }
+
+  login() {
+    this.authService.authenticate(this.username, this.password)
+  }
+
+  ngOnInit(): void {
+  }
+}
