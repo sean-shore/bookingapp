b9db65320c5f693a5beb3f570ecfe90d790ac8d2
seanshore <sshore@resharmonics.co.uk>
2021-03-23 12:49:52 +0000

    20 march

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NewEnquiryComponent } from './new-enquiry/new-enquiry.component';
@@ -22,8 +22,9 @@ import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatChipsModule} from "@angular/material/chips";
 import {MAT_DATE_LOCALE, MatNativeDateModule} from '@angular/material/core';
 import {LoginFormComponent } from './login-form/login-form.component';
-import { AuthModule } from '@auth0/auth0-angular';
+import {AuthModule } from '@auth0/auth0-angular';
 import {RouterModule} from "@angular/router";
+import {AuthService} from "./auth.service";
 
 @NgModule({
   declarations: [
@@ -52,8 +53,10 @@ import {RouterModule} from "@angular/router";
     MatNativeDateModule,
     AuthModule,
     RouterModule,
+    FormsModule,
   ],
   providers: [
+    AuthService,
     MatDatepickerModule,
     {provide: MAT_DATE_LOCALE, useValue: 'en-GB'}],
   bootstrap: [AppComponent]
--- src/app/services/api.constant.ts
+++
@@ -1,6 +0,0 @@
-export const ApiConstant: any = {
-  rootEndpoint: 'https://app.rerumapp.uk',
-  xAuthToken: 'x-auth-token',
-  baseUrl: 'k-base-url',
-  slash: '/',
-};
