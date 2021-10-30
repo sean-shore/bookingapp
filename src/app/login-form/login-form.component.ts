6da277ae0eab12b6fb0094382915f29513d9c167
seanshore <sshore@resharmonics.co.uk>
2021-10-30 16:38:37 +0100

    25/10/21

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, FormsModule} from "@angular/forms";
 import {Router} from "@angular/router";
-import {AuthService} from "../auth.service";
+import {AuthService} from "../../api-services/auth.service";
 
 
 @Component({
