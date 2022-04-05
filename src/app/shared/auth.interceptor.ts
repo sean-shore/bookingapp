4f59cfb0972638e697a33c82044986868988806f
seanshore <sshore@resharmonics.co.uk>
2022-04-05 08:52:05 +0100

    2022-04-05

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS, HttpEvent, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpEvent, HttpHandler } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { AuthService} from './auth.service';
@@ -9,19 +9,13 @@ export class AuthInterceptor implements HttpInterceptor {
 
   constructor(private authUser: AuthService) {
   }
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let authReq = req;
-    const authToken = this.authUser.getToken();
-    console.log(authToken);
-    if (authToken != null) {
-      authReq = req.clone({
-        headers: req.headers.set('Authorization', 'Bearer ' + authToken)
-      });
-    }
-    return next.handle(req);
-  }
-}
 
-export const authInterceptorProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
-];
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const userToken = this.authUser.getToken();
+    const modifiedReq = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${userToken}`),
+    });
+    return next.handle(modifiedReq);
+  }
+
+}
